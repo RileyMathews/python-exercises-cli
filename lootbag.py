3269a6266ebcd18ecc6ea0a0d5c3c75bd5d9301d
Riley Mathews <rileymathews80@gmail.com>
2018-07-13 14:40:02 -0500

    added initial tests

---
+++ lootbag.py
@@ -0,0 +1,17 @@
+
+class LootBag:
+
+    def add_child(self):
+        return True
+
+    def add_loot(self):
+        return True
+
+    def remove_loot(self):
+        return True
+
+    def get_children(self):
+        return True
+
+    def get_childs_loot(self):
+        return True
--- tests/test_lootbag.py
+++ tests/test_lootbag.py
@@ -1,9 +1,17 @@
+import sys
 import unittest
+
+from lootbag import LootBag
+
+sys.path.append('../')
 
 
 class LootbagTest(unittest.TestCase):
     """testing class for the lootbag class"""
 
     def test_childrens_toys_can_be_added_to_bag(self):
-        loot_bag = Bag()
-        loot_bag.add_toy_for_child("Billy", "xbox")
+        loot_bag = LootBag()
+        self.assertTrue(loot_bag.add_loot())
+
+if __name__ == '__main__':
+    unittest.main()
